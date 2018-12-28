cff73fa5d06c978e495aa1fd4895b2c1a12e448d
Jeff Benton <sikletz@gmail.com>
2018-12-28 03:29:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(str)
   if str == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif str.match('[^A-Z]') != nil
+  elsif str.match('[^A-Z][ ,?,!,.]') != nil
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
