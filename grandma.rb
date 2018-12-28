3ec9898345a721488ba46e3a9218a72e7b83bd93
Jeff Benton <sikletz@gmail.com>
2018-12-28 03:13:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(str)
   if str == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif !str.match('[A-Z]')
     puts "HUH?! SPEAK UP, SONNY!"
   else
